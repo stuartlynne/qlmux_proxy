5f931a6ba2cc0a9b032d3894ac0094244e293478
Stuart Lynne <stuart.lynne@gmail.com>
2018-09-12 15:28:03 -0700

    get rid of printer.status

--- qlmux/printer.py
+++ qlmux/printer.py
@@ -45,7 +45,6 @@ class Printer( object ):
 		self.name = name
 		self.testport = testport
 		self.model = model
-		self.status = PrinterStatus.UNKNOWN
 		self.snmpstatus = SNMPStatus.UNKNOWN
 		self.snmpvalue = ''
 		self.snmpmedia = ''
@@ -120,10 +119,6 @@ class Printer( object ):
 		self.printjobs.append(Job(pool, data))
 		return
 
-	def status(self):
-		print('value: %s' % status.value)
-		return self.status
-
 	# check if there are jobs queued to this printer and we are currently active
 	#
 	def checkforjobs(self):
@@ -174,7 +169,7 @@ class Printer( object ):
                         self.jobsfinished += 1
 
 	def __repr__(self):
-		return "Printer[%s] status: %s snmpmodel: %s snmpstatus: %s snmpmedia: %s printjobs: %d\n" % (
-			self.name, self.status, self.snmpmodel, self.snmpstatus, self.snmpmedia, len(self.printjobs))
+		return "Printer[%s] snmpmodel: %s snmpstatus: %s snmpmedia: %s printjobs: %d\n" % (
+			self.name, self.snmpmodel, self.snmpstatus, self.snmpmedia, len(self.printjobs))
 
 
