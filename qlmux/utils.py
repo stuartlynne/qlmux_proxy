445fb5e33814fcd86144ab20bfb09f51244ff19a
Stuart Lynne <stuart.lynne@gmail.com>
2021-08-10 23:36:56 -0700

    log to stderr

--- qlmux/utils.py
+++ qlmux/utils.py
@@ -1,10 +1,11 @@
 
 
 
+import sys
 import datetime
 getTimeNow = datetime.datetime.now
 
 
 def log(s):
-        print('%s %s' % (getTimeNow().strftime('%H:%M:%S'), s.rstrip()))
+        print('%s %s' % (getTimeNow().strftime('%H:%M:%S'), s.rstrip()), file=sys.stderr)
 
