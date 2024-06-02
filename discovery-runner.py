6d5807fe3a724300d069804b428a1f1d1c9701be
Stuart Lynne <stuart.lynne@gmail.com>
2024-06-02 13:57:16 -0700

    runner for discovery testing

---
+++ discovery-runner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""Convenience wrapper for running bootstrap directly from source tree."""
+
+
+from qlmux.discovery import discoveryMain
+
+
+if __name__ == '__main__':
+    discoveryMain()
