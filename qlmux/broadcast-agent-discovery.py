9f91cb15e57015e2e48e3a6a3ad58b44259d782d
root <stuart.lynne@gmail.com>
2024-11-04 15:29:49 -0800

    pysnmp 7.1.9 broadcast example

--- qlmux/broadcast-agent-discovery.py
+++ qlmux/broadcast-agent-discovery.py
@@ -9,9 +9,9 @@ Send SNMP GET request to broadcast address and wait for respons(es):
 * to all Agents via broadcast address 255.255.255.255:161
 * for OIDs in tuple form
 
-Here we send out a single SNMP request and wait for potentially many SNMP 
+Here we send out a single SNMP request and wait for potentially many SNMP
 responses from multiple SNMP Agents listening in local broadcast domain.
-Since we can't predict the exact number of Agents responding, this script 
+Since we can't predict the exact number of Agents responding, this script
 just waits for arbitrary time for collecting all responses. This technology
 is also known as SNMP-based discovery.
 
@@ -19,91 +19,78 @@ This script performs similar to the following Net-SNMP command:
 
 | $ snmpget -v2c -c public -ObentU 255.255.255.255 1.3.6.1.2.1.1.1.0 1.3.6.1.2.1.1.3.0
 
-"""#
-from pysnmp.carrier.asyncore.dispatch import AsyncoreDispatcher
-from pysnmp.carrier.asyncore.dgram import udp
+"""  #
+import sys
+from pysnmp.carrier.asyncio.dispatch import AsyncioDispatcher
+from pysnmp.carrier.asyncio.dgram import udp
 from pyasn1.codec.ber import encoder, decoder
 from pysnmp.proto import api
-from time import time
 
 # Broadcast manager settings
 maxWaitForResponses = 5
 maxNumberResponses = 10
 
 # Protocol version to use
-# pMod = api.protoModules[api.protoVersion1]
+# pMod = api.PROTOCOL_MODULES[api.SNMP_VERSION_1]
 pMod = api.PROTOCOL_MODULES[api.SNMP_VERSION_2C]
 
 # Build PDU
 reqPDU = pMod.GetRequestPDU()
-pMod.apiPDU.setDefaults(reqPDU)
-pMod.apiPDU.setVarBinds(
-    reqPDU, (('1.3.6.1.2.1.1.1.0', pMod.Null('')),
-             ('1.3.6.1.2.1.1.3.0', pMod.Null('')))
+pMod.apiPDU.set_defaults(reqPDU)
+pMod.apiPDU.set_varbinds(
+    reqPDU, (("1.3.6.1.2.1.1.1.0", pMod.Null("")), ("1.3.6.1.2.1.1.3.0", pMod.Null("")))
 )
 
 # Build message
 reqMsg = pMod.Message()
-pMod.apiMessage.setDefaults(reqMsg)
-pMod.apiMessage.setCommunity(reqMsg, 'public')
-pMod.apiMessage.setPDU(reqMsg, reqPDU)
-
-startedAt = time()
-
-
-class StopWaiting(Exception): pass
-
-
-def cbTimerFun(timeNow):
-    if timeNow - startedAt > maxWaitForResponses:
-        raise StopWaiting()
+pMod.apiMessage.set_defaults(reqMsg)
+pMod.apiMessage.set_community(reqMsg, "public")
+pMod.apiMessage.set_pdu(reqMsg, reqPDU)
 
 
 # noinspection PyUnusedLocal,PyUnusedLocal
-def cbRecvFun(transportDispatcher, transportDomain, transportAddress,
-              wholeMsg, reqPDU=reqPDU):
-
+def __callback(
+    transportDispatcher, transportDomain, transportAddress, wholeMsg, reqPDU=reqPDU
+):
+    print("Received response from %s" % (transportAddress,), file=sys.stderr)
     while wholeMsg:
         rspMsg, wholeMsg = decoder.decode(wholeMsg, asn1Spec=pMod.Message())
-        rspPDU = pMod.apiMessage.getPDU(rspMsg)
-
+        rspPDU = pMod.apiMessage.get_pdu(rspMsg)
         # Match response to request
-        if pMod.apiPDU.getRequestID(reqPDU) == pMod.apiPDU.getRequestID(rspPDU):
-
+        if pMod.apiPDU.get_request_id(reqPDU) == pMod.apiPDU.get_request_id(rspPDU):
             # Check for SNMP errors reported
-            errorStatus = pMod.apiPDU.getErrorStatus(rspPDU)
+            errorStatus = pMod.apiPDU.get_error_status(rspPDU)
             if errorStatus:
-                print(errorStatus.prettyPrint())
-
+                print(errorStatus.prettyPrint(), file=sys.stderr)
             else:
-                for oid, val in pMod.apiPDU.getVarBinds(rspPDU):
-                    print('%s = %s' % (oid.prettyPrint(), val.prettyPrint()))
-
-            transportDispatcher.jobFinished(1)
-
+                for oid, val in pMod.apiPDU.get_varbinds(rspPDU):
+                    print(f"{oid.prettyPrint()} = {val.prettyPrint()}", file=sys.stderr)
+            transportDispatcher.job_finished(1)
     return wholeMsg
 
 
-transportDispatcher = AsyncoreDispatcher()
+transportDispatcher = AsyncioDispatcher()
 
-transportDispatcher.registerRecvCbFun(cbRecvFun)
-transportDispatcher.registerTimerCbFun(cbTimerFun)
+transportDispatcher.register_recv_callback(__callback)
 
 # UDP/IPv4
-udpSocketTransport = udp.UdpSocketTransport().openClientMode().enableBroadcast()
-transportDispatcher.registerTransport(udp.DOMAIN_NAME, udpSocketTransport)
+udpSocketTransport = udp.UdpAsyncioTransport().open_client_mode(allow_broadcast=True)
+transportDispatcher.register_transport(udp.DOMAIN_NAME, udpSocketTransport)
 
 # Pass message to dispatcher
-transportDispatcher.sendMessage(
-    encoder.encode(reqMsg), udp.DOMAIN_NAME, ('255.255.255.255', 161)
+print("Sending request: %s" % (encoder.encode(reqMsg),), file=sys.stderr)
+transportDispatcher.send_message(
+    encoder.encode(reqMsg), udp.DOMAIN_NAME, ("255.255.255.255", 161)
 )
-
+print("job started", file=sys.stderr)
 # wait for a maximum of 10 responses or time out
-transportDispatcher.jobStarted(1, maxNumberResponses)
+transportDispatcher.job_started(1, maxNumberResponses)
 
+print("Run dispatcher", file=sys.stderr)
 # Dispatcher will finish as all jobs counter reaches zero
 try:
-    transportDispatcher.runDispatcher()
-
+    transportDispatcher.run_dispatcher(maxWaitForResponses)
+except:
+    raise
 finally:
-    transportDispatcher.closeDispatcher()
+    transportDispatcher.close_dispatcher()
